d907f141972c2d6ba2dd0b647a8ab4f1a2e068a5
rohit <rohit@fundsindia.com>
2018-09-02 18:43:31 +0530

    Amicable numbers removed storage

--- 21_amicable_numbers.py
+++ 21_amicable_numbers.py
@@ -6,10 +6,9 @@ start = time.time()
 factorsDict = {}
 amicableNumbers = set()
 for x in range(2, 10001):
-    factorsDict[x] = sum(list(findFactors(x) - {x}))
-    for key in factorsDict:
-        if factorsDict[key] == x and key != x and factorsDict[x] == key:
-            amicableNumbers = amicableNumbers | { key , x}
+    sumx = sum(list(findFactors(x) - {x}))
+    if (sum(list(findFactors(sumx) - {sumx}))) == x:
+        amicableNumbers | {x, sumx}
 
 
 print(sum(list(amicableNumbers)))
