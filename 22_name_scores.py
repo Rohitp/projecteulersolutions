153823d79a3ccf83a8099be846683f8e8941a6de
rohit <rohit@fundsindia.com>
2018-09-02 19:54:45 +0530

    Made 22 into a one liner. Readability be damned

--- 22_name_scores.py
+++ 22_name_scores.py
@@ -3,20 +3,9 @@ import time
 
 start = time.time()
 
-file = open("22_name_scores.txt", "r")
-names = []
-
 # He we don't bother converting to uppercase as we assume the input is sanitized
 
-for line in file:
-    names.extend(line.replace('"',"").split(","))
-
-names.sort()
-
-score = sum([ x * (i+1) for i,x in enumerate(list(map(lambda x: reduce(lambda c, d: (ord(c) - 64) + (ord(d) - 64) if not isinstance(c, int) else c + (ord(d) - 64), list(x)) , names))) ])
-
-
-print(score)
+print(sum([ x * (i+1) for i,x in enumerate(list(map(lambda x: reduce(lambda c, d: (ord(c) - 64) + (ord(d) - 64) if not isinstance(c, int) else c + (ord(d) - 64), list(x)) , sorted(open("22_name_scores.txt", "r").readline().replace('"',"").split(",")) ))) ]))
 
 end = time.time()
 print("Time elapsed "+ str(end - start))
